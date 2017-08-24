0445061e4a614a1022d4ab9c66d19881af2398a4
philswan <mail@philswan.co.uk>
2017-08-24 10:32:36 +0100

    Updates to eligibility

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Access to Work (exploration)',
+  serviceName: 'Access to Work exploration',
 
   // Default port that prototype runs on
   port: '3000',
